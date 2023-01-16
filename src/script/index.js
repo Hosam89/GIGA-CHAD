ebced3ba7da0a284f9c105828e77c922dca599ae
Hosam <108076964+Hosam89@users.noreply.github.com>
2023-01-16 12:00:17 +0100

    albun fetch done

--- src/script/index.js
+++ src/script/index.js
@@ -9,6 +9,12 @@ const searchContainer =  document.querySelector('#search-container')
 const mainArtistImage = document.querySelector('#artist-pic');
 const mainArtistName = document.querySelector('#artist-name');
 const mainLinstSong = document.querySelectorAll('.songs');
+
+const artistArray = ['petergabriel' , 'genesis' ,'bonnietyler' , 'barrywhite' , 'againstthemall' , 'selenagomez','ninachuba' , 'theweekend' , 'calumscott' , 'vitas' , 'annenmaykantereit' , 'wafikhabib' , 'myriamfares' , 'asereje']
+const randomNumber = Math.floor(Math.random() * artistArray.length);
+
+const albumContainer = document.querySelector('#album-cards')
+
 window.onload =async ()=>{
     const lastSearched = await getData(`search?q=${localStorage.getItem('searched')}`);
     const artistInfo = await lastSearched.data
@@ -39,7 +45,7 @@ homebutton.addEventListener('click' , ()=>{
      
         artist_cards.classList.add('d-none');
        searchContainer.classList.remove('d-none');
-
+       cardsContainer.classList.add('d-flex');
         searchInput.addEventListener("change", function() {
 
         // Get the value of the input
@@ -73,7 +79,7 @@ homebutton.addEventListener('click' , ()=>{
                 right.style.marginTop = '4rem';
                 right.style.marginLeft = '2rem';
            
-                right.classList.add('d-felx' , 'justify-content-center' , 'align-items-center')
+                right.classList.add('d-flex' , 'justify-content-center' , 'align-items-center')
                 const card = document.createElement('div');
                 card.classList.add('card' , 'text-center');
                 card.style.width = '330px';
@@ -87,10 +93,10 @@ homebutton.addEventListener('click' , ()=>{
                 const card_song_atrist = document.createElement('p');
                 card_song_atrist.innerText = artistInfo[0].artist.name
                 const playList = document.createElement('ul');
-                playList.classList.add('list-group')
+                playList.classList.add('songlist')
                 const playListItem = document.createElement('li')
                 for(let i = 0 ; i<=5 ; i++){
-                    playList.innerHTML+= `<li class="songs d-flex"> <img src='${artistInfo[i].artist.picture_xl}' class="album-pic"> <a href="${artistInfo[i].preview}" target="_blank"> ${artistInfo[i].title}</br><span class="title">${artistInfo[i].artist.name}</span></a> </li>`
+                    playList.innerHTML+= `<li class="songs d-flex"> <img src='${artistInfo[i].artist.picture_xl}' class="album-pic"> <a href="${artistInfo[i].preview}" target="_blank" style="text-decoration: none;" class ="text-light"> ${artistInfo[i].title}</br><span class="title">${artistInfo[i].artist.name}</span></a> </li>`
                     // playListItem.innerText += artistInfo[i].title
                     // playList.append(playListItem)
                 }
@@ -106,3 +112,72 @@ homebutton.addEventListener('click' , ()=>{
   
  })
 
+ const randomFive = (arr) => {
+    let shuffel = arr.sort(() => 0.5 - Math.random());
+    return shuffel.slice(0, 5);
+  }; 
+
+
+
+  const randomArtist = randomFive(artistArray);
+  
+(async ()=>{
+    cardsContainer.classList.remove('d-flex');
+    albumContainer.classList.add('gap-3' , 'row');
+    albumContainer.style.marginTop = '4.5rem';
+    albumContainer.style.paddingLeft = '14.5px'
+  albumContainer.style.width = '100%'
+    for(let i = 0 ; i < randomArtist.length ; i++){  
+        const data = await getData(`search?q=${randomArtist[i]}`);
+        const album = await data.data;
+        console.log(album);
+
+        const card = document.createElement('div');
+        card.classList.add('card' , 'col-xl-3' ,'col-md-6' , 'col-sm-12' , 'text-center')
+        card.style.height = '550px';
+        card.style.width = '19%';
+        card.style.paddingTop = '10px';
+        card.style.fontSize = '2rem'
+        const card_body = document.createElement('div');
+        card_body.classList.add('card-body')
+        
+
+
+        const albumImg = document.createElement('img');
+        albumImg.classList.add('card-img-top')
+        albumImg.src = album[i].album.cover_big
+        albumImg.style.width = "100%"
+        albumImg.classList.add('album-pic')
+
+
+        const albumName = document.createElement('h3');
+      
+        albumName.innerText = album[i].album.title;
+        
+        
+        
+        
+        
+        const artistName = document.createElement('p');
+     
+        artistName.textContent = album[i].artist.name;
+
+        card_body.append(albumName , artistName);
+        card.append(albumImg , card_body);
+        albumContainer.append(card)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+    }
+ })()
