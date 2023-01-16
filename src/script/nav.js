33a3a772e4cca3c885dfb217f948c6d8c49f2ba9
Ali Rusta <ali_rusta@web.de>
2023-01-16 09:49:26 +0100

    changed songlist to class, added nav.js code

--- src/script/nav.js
+++ src/script/nav.js
@@ -1,49 +1,29 @@
-// const sidenav = document.getElementById('sidenav');
-// const navBtn = document.getElementById('mobile-toggle-btn');
-// let visible = null; 
-
-// navBtn.addEventListener('click', () => {
-//     if (window.innerWidth < 451) {
-//     if (visible !== true) {
-//         sidenav.style.transform = 'translateX(100%)';
-//         visible = true;
-//     } else {
-//         sidenav.style.transform = 'translateX(200%)';
-//         visible = null; 
-//     };
-//     }
-// });
-
 const sideNav = document.getElementById('sidenav');
 const navBtn = document.getElementById('mobile-toggle-btn');
 const logoLine = document.getElementById('logo-line');
 let visible = false; 
 
-// navBtn.addEventListener('click', () => {
-//     if (window.innerWidth < 451) {
-//         if (!visible) {
-//             sidenav.style.transform = 'translateX(0%)';
-//             sidenav.style.display = 'block';
-//             visible = true;
-//         } else {
-//             sidenav.style.transform = 'translateX(-100%)';
-//             visible = false; 
-//         }
-//     } else return;
-// });
-
 navBtn.addEventListener("click", () => {
   visible = !visible;
   sideNav.classList.toggle("active");
 });
-
-window.addEventListener("resize", () => {
-  if (window.innerWidth >= 451) {
+const smallscreen = 450
+window.addEventListener("load", () => {
+  if (window.innerWidth > smallscreen) {
     sideNav.classList.add("active");
     logoLine.classList.toggle("active");
+    sideNav.style.transition = 'none';
     visible = true;
   } else {
     sideNav.classList.remove("active");
     visible = false;
+    sideNav.style.transition = 'transform 0.3s ease-in-out'
+    // transition: transform 0.3s ease-in-out;
   }
 });
+
+window.addEventListener('resize' , ()=>{
+  if(window.innerWidth > smallscreen){
+    sideNav.classList.toggle('active')
+  }
+})
